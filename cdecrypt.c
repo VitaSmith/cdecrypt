67dbccdc4695a1237016a5c64a5c297186cac5b9
VitaSmith <vitasmith@rpc1.org>
2022-10-24 13:04:29 +0100

    Update solution files to VS2022

--- cdecrypt.c
+++ cdecrypt.c
@@ -2,7 +2,7 @@
   cdecrypt - Decrypt Wii U NUS content files
 
   Copyright © 2013-2015 crediar <https://code.google.com/p/cdecrypt/>
-  Copyright © 2020-2021 VitaSmith <https://github.com/VitaSmith/cdecrypt>
+  Copyright © 2020-2022 VitaSmith <https://github.com/VitaSmith/cdecrypt>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -366,7 +366,7 @@ int main_utf8(int argc, char** argv)
 
     if (argc < 2) {
         printf("%s %s - Wii U NUS content file decrypter\n"
-            "Copyright (c) 2020-2021 VitaSmith, Copyright (c) 2013-2015 crediar\n"
+            "Copyright (c) 2020-2022 VitaSmith, Copyright (c) 2013-2015 crediar\n"
             "Visit https://github.com/VitaSmith/cdecrypt for official source and downloads.\n\n"
             "Usage: %s <file or directory>\n\n"
             "This program is free software; you can redistribute it and/or modify it under\n"
